145f77ee4bd21a42eba1e4da97f83c8612844509
zhoupeng <zhoupengzy@163.com>
2020-06-03 18:28:14 +0800

    add btnstart

--- Assets/Scripts/DealCard.cs
+++ Assets/Scripts/DealCard.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DealCard : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Button BtnStart;
+
+    public Button BtnReset;
+
+    public Image Card1;
+    public Image Card2;
+    public Image Card3;
+    public Image Card4;
+    public Image Card5;
+
+    public Transform PosEnd1;
+
+    // Use this for initialization
+    void Start () {
+         BtnStart.onClick.AddListener(OnClickStart);
+         BtnStart.onClick.AddListener(OnClickReset);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+    }
+
+    public void OnClickStart()
+    {
+        Debug.Log("OnClickStart");
+    }
+
+    public void OnClickReset()
+    {
+
+    }
 }
